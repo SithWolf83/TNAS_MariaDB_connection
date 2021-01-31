5e8bf205f088742ef9020c6b13082930ba835964
SithWolf83 <ivan.ferrer1@gmail.com>
2021-01-31 09:47:28 +0100

    Create basic project with Spring Initializer.

---
+++ WebScrapingDB/src/main/java/com/iifg/WebScrapingDB/WebScrapingDbApplication.java
@@ -0,0 +1,13 @@
+package com.iifg.WebScrapingDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScrapingDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScrapingDbApplication.class, args);
+	}
+
+}
---
+++ WebScrapingDB/src/test/java/com/iifg/WebScrapingDB/WebScrapingDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iifg.WebScrapingDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScrapingDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
