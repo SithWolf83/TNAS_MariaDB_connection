c36234d77d8941e58e08be722202c575c9fb428e
SithWolf83 <ivan.ferrer1@gmail.com>
2021-02-12 02:34:49 +0100

    Resolved error.

--- WebScrapingDB/src/main/java/com/iifg/WebScrapingDB/repositories/TravellerRecommendRepository.java
+++ WebScrapingDB/src/main/java/com/iifg/WebScrapingDB/repositories/TravellerRecommendRepository.java
@@ -3,7 +3,9 @@ package com.iifg.WebScrapingDB.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.iifg.WebScrapingDB.entities.TravellerRecommendations;
+
 @Repository
-public interface TravellerRecommendRepository extends CrudRepository<TravellerRecommendRepository, Integer> {
+public interface TravellerRecommendRepository extends CrudRepository<TravellerRecommendations, Integer> {
 
 }
